5b638d6a6d6a9173cdd36a59d8011df95296af53
kishoregowthaman <kg9417@srmist.edu.in>
2025-10-07 20:28:48 +0530

    Add detailed README for JAVA-CRASH-COURSE repository

---
+++ module-01-basics/Comments.java
@@ -0,0 +1,10 @@
+public class Comments {
+    public static void main(String[] args) {
+        // => Used for single-line comments
+    
+        /* => Used for multi-line comments
+         *    "Find oppurtunity in failure"
+         */
+        
+    }
+}
